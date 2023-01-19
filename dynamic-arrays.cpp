133c2ff45df4c4c6ca12934ea1d82adc163701ed
HazelDaniel <hazeldaniel856@gmail.com>
2023-01-19 16:00:04 -0600

    more on pointers

--- dynamic-arrays.cpp
+++ dynamic-arrays.cpp
@@ -1,86 +1,106 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
+using std::vector;
+using std::string;
+using std::endl;
+using std::cout;
+using std::cin;
 
-void add_numbers (int &arrlen);
+int* add_to_array (int arr_len);
+void print_array (int* const array_ptr);
+
 
 int main (){
 
-    char input_char {};
-    // int* global_pointer {nullptr};
-    int arrlen {0};
+    // string input_text {};
+    // string selection {};
+
+    cout << std::boolalpha;
+    cout << "\n------------------ WELCOME TO DYNAMIC ARRAYS!!! ------------\n";
+    cout << "\a|\t\t\t\t\t\t\t\t|\n-----------------------------------------------------------------\n";
+
+    int* array_ptr {nullptr};
+
+    int arr_len{0};
+    cout << "How many elements do you want to add ? : ";
+    cin >> arr_len;
+    array_ptr = add_to_array(arr_len);
+
+    int init_len{0};
+    cout << "first three numbers are : " << endl;
+    while(init_len < arr_len){
+        cout << array_ptr[init_len] << " ";
+        init_len ++;
+    }
+
+    delete [] array_ptr;
+
+    // do{
+    //     cout << "\tEnter: \n";
+    //     cout << "\tA => Add array numbers\n";
+    //     cout << "\tP => Print array numbers\n";
+    //     cout << "\tQ => quit\n";
+    //     cout << "\t================= your selection: ";
+    //     getline(cin, selection);
+
+    //     char selected{selection.at(0)};
+
+    //     switch (selected){
+    //         case 'Q':
+    //         case 'q':
+    //             // this is where you free up the space. 
+    //             delete [] array_ptr;
+    //             cout << "Goodbye...";
+    //             break;
+    //         case 'A':
+    //         case 'a':
+    //             {
+    //                 int arr_len{0};
+    //                 cout << "How many elements do you want to add ? : ";
+    //                 cin >> arr_len;
+    //                 array_ptr = add_to_array(arr_len); // bad practice ! this will cause memory leak .
+    //             }
+    //             break;
+    //         case 'P':
+    //         case 'p':
+
+    //             break;
+
+    //         default:
+    //             cout << "\t\t\t\t\t\t\t\t\tInvalid Option! " << selected << " try again. " << endl;
+    //     }
 
 
-    do{
-        cout << "P - Print Numbers\n";
-        cout << "A - Add numbers\n";
-        cout << "M - Display mean of the numbers\n";
-        cout << "S - Display the smallest number\n";
-        cout << "L - Display the largest number\n";
-        cout << "Q/q - Quit\n\n";
-        cout << "=========================\n";
-
-        cin >> input_char;
-
-        switch(input_char){
-            case 'p':
-            case 'P':
-                break;
-            case 'a':
-            case 'A':
-                add_numbers(arrlen);
-            break;
-            case 'M':
-            case 'm':
-                break;
-            case 'S':
-            case 's':
-                break;
-            case 'l':
-            case 'L':
-
-                break;
-            case 'q':
-            case 'Q':
-                break;
-            default:
-                cout << "invalid input . try again!" << input_char << endl;
-                break;
-
-        }
-
-    }while(input_char != 'q' && input_char != 'Q');
-
-    cout << "Goodbye!" << endl;
+    // }while(selection.at(0) != "Q"[0] && selection.at(0) != "q"[0] && selection.at(0));
 
     return 0;
 
 }
-void add_numbers (int &arrlen){
-    cout << "How many numbers are you adding? : ";
 
-    int arrel {};
-    int initlen {};
 
-    cin >> arrlen;
+int* add_to_array (int arr_len){
 
-    int* array_ptr = new int[arrlen];
+    int* new_arr_ptr {nullptr};
 
-    while(initlen < arrlen){
-        cout << "enter array element : ";
-        cin >> arrel;
-        array_ptr[initlen] = arrel;
-        initlen++;
+    new_arr_ptr = new int[arr_len];
+
+    int arr_ind {0};
+
+    while (arr_ind < arr_len){
+        cout << "enter array element : " ;
+        cin >> *(new_arr_ptr + arr_ind);
+        arr_ind++;
     }
 
+    cout << "insertion complete! enter [P] to show array element." << endl;
+    return new_arr_ptr;
+}
 
-    cout << "first three elements are : " << endl;
-    
-    cout << array_ptr[0] << endl;
-    cout << array_ptr[1] << endl;
-    cout << array_ptr[2] << endl;
+void print_array (int* const array_ptr){
 
-    delete [] array_ptr;
-
+    cout << "first three numbers are : " << endl;
+    cout << array_ptr [0] << array_ptr[1] << array_ptr[2] << endl;
 
 }
